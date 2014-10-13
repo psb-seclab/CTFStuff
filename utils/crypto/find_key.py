ee83c28ce6a45ed320f1c2cd24f335443d68bd96
klonman <klonman@gmail.com>
2014-10-13 17:05:09 -0400

    update

---
+++ utils/crypto/find_key.py
@@ -0,0 +1,29 @@
+from Crypto.Cipher import AES
+import os, random, struct
+
+# you may modify this script to accomplish drill 21
+# make sure you pad the key using '\x20' to make it 16 bytes
+
+word = 10*'a' # this is read from the word list!
+padsize = 6 # this is determined by the word size
+key = word + padsize*'\x20'
+IV = 16 * '\x00'           
+mode = AES.MODE_CBC
+encryptor = AES.new(key, mode, IV=IV)
+
+# !!!
+# The text has to be padded as well sincie it is not a multiple of 16!!
+# we use the PKCS5 padding scheme
+# the length of plain text is 21, to make it 32 = 2*16, we need to pad 11 bytes
+# in hex 11 = 0b
+text = "This is a top secret." + 11*'\x0b'
+
+print text
+ciphertext = encryptor.encrypt(text)
+
+print ciphertext.encode('hex')
+
+decryptor = AES.new(key, mode, IV=IV)
+plain = decryptor.decrypt(ciphertext)
+
+print plain
---
+++ utils/py_main.py
@@ -0,0 +1,26 @@
+"""Module docstring.
+
+This serves as a long usage message.
+"""
+import sys
+import getopt
+
+def main():
+    # parse command line options
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
+    except getopt.error, msg:
+        print msg
+        print "for help use --help"
+        sys.exit(2)
+    # process options
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            print __doc__
+            sys.exit(0)
+    # process arguments
+    for arg in args:
+        process(arg) # process() is defined elsewhere
+
+if __name__ == "__main__":
+    main()
