8aa218a7af7254e9a5fa4d410f00f4a928823a14
klonman <klonman@gmail.com>
2014-10-16 11:21:39 -0400

    update

--- utils/python_101/python_101.py
+++ utils/python_101/python_101.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+#import my_math
+from my_math import factorial
+#import my_math
+
+
 def test_db():
 	
 	return
@@ -8,38 +13,226 @@ def test_network():
 	
 	return
 
+def test_exception():
+	# opening file failed
+	try:
+		fi = open("testfile", 'r')
+   		fh = open("testfile", "w")
+   		fh.write("This is my test file for exception handling!!")
+	except IOError:
+   		print "Error: can\'t find file or read data"
+	else:
+   		print fi.read()
+   		print "Written content in the file successfully"
+   		fh.close()
+   		fi.close()
+	return
+
+def test_module():
+	print '10! = %d'%(factorial(10))
+	return
+
+class Employee:
+   'Common base class for all employees'
+   empCount = 0
+
+   def __init__(self, name, salary):
+      	self.name = name
+      	self.salary = salary
+      	Employee.empCount += 1
+   
+   def displayCount(self):
+     	print "Total Employee %d" % Employee.empCount
+
+   def displayEmployee(self):
+      	print "Name : ", self.name,  ", Salary: ", self.salary
+
+
 
 def test_class():
+	"This would create first object of Employee class"
+	emp1 = Employee("Zara", 2000)
+	"This would create second object of Employee class"
+	emp2 = Employee("Manni", 5000)
+	emp1.displayEmployee()
+	emp2.displayEmployee()
+	print "Total Employee %d" % Employee.empCount
+	print emp1.empCount
+	# inheritence
+	# overiding
+	# operator overloading
+
 	return
 
+
+def fib_1(n):
+	"""Print a Fibonacci series up to n."""
+	a, b = 0, 1
+	while b < n:
+		print b
+		a, b = b, a+b
+	return
+
+cnt = 0
+fib_tmp = {}# make fib faster
+def fib_2(n):
+	"""return the nth fib num"""
+	global cnt
+	cnt += 1
+	if n == 0:
+		return 0
+	elif n == 1:
+		return 1
+	elif n > 1:
+		return fib_2(n-1) + fib_2(n-2)
+	else:
+		print 'invalid input'
+		return None
+
+def simple_func(a, b, c):
+	return a + b + c**3
 
 def test_function():
+	print simple_func(1, 2, 3)
+	fib_1(100)
+	print fib_2(5)
+	print 'fib_2 is called %d times'%(cnt)
 	return
 
-def test_io():
+def test_generator():
+	l1 = range(100)
+	print l1
+	# the first 100 odd numbers
+	l2 = [2*x+1 for x in range(100)]
+	print l2
+	# gen a dict
+	# gen a ascii code table
+	dict1 = {x:chr(x) for x in range(128)}
+	print dict1
+	# gen a 10*10 array
+	l3 = [[10*x+y for y in range(10)] for x in range(10)]
+	print l3
+	# cross product
+	vec1 = [2, 4, 6]
+	vec2 = [1, 3, 5]
+	cross_product = [x*y for x in vec1 for y in vec2]
+	print cross_product
+	# using if
+	vec_if = [x for x in l1 if x % 7 == 0]
+	print vec_if
+	print len(vec_if)
+	return
 
+
+def test_file_io():
+	# write to a file
+	fo = open('testfile', 'wt')
+	for x in range(100):
+		fo.write(str(x)+'\n')
+	fo.close()
+	# read from a file
+	fi = open('testfile', 'rt')
+	# read as much as possible at one time!
+	contents = fi.read()
+	print contents
+	
+	return
+
+
+def test_io():
+	# print function
+	a = ['hello', 'this is fun', 'I love wargames']
+	for item in a:
+		print item, len(item)
+	# get input from keyboard
+	# raw_input, get a line of input from keyboard as string
+	x = str(raw_input("enter something:"))
+	print x
+	# input
+	x = input("input your python expression: ")
+	print x
 	return
 
 def test_loops():
+	# for loops, break, continue
+	# problem: check prime
+	n = 23
+	prime = True
+	for x in range(2, n):
+		if n % x == 0:
+			print '%d is not a prime since it has a factor %d'%(n, x)
+			prime = False
+			break
+	if prime:
+		print '%d is a prime'%(n)
+	# using while loop do the same
+	prime = True
+	x = 2
+	while x < n:
+		if n % x == 0:
+			print '%d is not a prime since it has a factor %d'%(n, x)
+			prime = False
+			break
+		x += 1
+	if prime:
+		print '%d is a prime'%(n)
+	
+	# do while?
+	n = 1
+	while True:
+		if n < 10:
+			print n
+		n += 1
+	
 	return
 
 def test_control_flow():
 	# get input from keyboard
-	x = int(raw_input("Please enter #:"))
+	#x = int(raw_input("Please enter #:"))
+	x = 5
 	if x < 0:
-	  x = 0
-	  print 'Negative changed to zero'
+		x = 0
+	  	print 'Negative changed to zero'
 	elif x == 0:
-	  print 'Zero'
+	  	print 'Zero'
 	elif x == 1:
-	  print 'Single'
+	  	print 'Single'
 	else:
-	  print 'More'
+	  	print 'More'
+	# no case statement
+
 	return
 
 
 def test_dictionary():
-	pass
+	# create a dictionary
+	dict_1 = {'Alice': '2341', 'Beth': '9102', 'Cecil': '3258'}
+	print dict_1
+	dict_2 = {x:x*'a' for x in range(10)}
+	print dict_2
+	# add a new entry
+	dict_1['newguy'] = '2323'
+	print dict_1
+	# del a entry
+	del dict_1['Beth']
+	print dict_1
+	# check for existance
+	print dict_1.has_key('Beth')
+	print 'Beth' in dict_1
+	print 'Alice' in dict_1
+	# update dict
+	print dict_1['Alice']
+	dict_1['Alice'] = '323232'
+	print dict_1['Alice']
+	# no duplicates!
+	# make a copy
+	copy_dict_1 = dict_1.copy()
+	print copy_dict_1
+	
+	# clear the dict
+	dict_1.clear()
+	print dict_1
+	return
 
 def test_list():
 	# items are ordered
@@ -84,6 +277,8 @@ def test_list():
 	print a
 	a.reverse()
 	print a
+	# index function
+	print index('spam')
 	return
 
 def test_str():
@@ -127,6 +322,9 @@ def test_var():
 	print chr(j)
 	print kk.encode('utf-8')
 	print d.encode('utf-8')
+	# the id function
+
+	# global var
 	return
 
 
@@ -135,6 +333,13 @@ if __name__ == "__main__":
     #test_str()
     #test_list()
     #test_dictionary()
-    test_control_flow()
-
-
+    #test_control_flow()
+    #test_loops()
+    #test_function()
+    #test_generator()
+    #test_module()
+    #test_io()
+    test_file_io()
+    #test_class()
+    #test_exception()
+    
