1fd8e36c0239b36eb5ca088c7385ce8ce3f798a7
klonman <klonman@gmail.com>
2014-10-15 22:57:16 -0400

    update

---
+++ utils/python_101/python_101.py
@@ -0,0 +1,18 @@
+
+
+a = ['spam', 'eggs', 100, 1234, 2*2]
+
+def test_var():
+	a = 5
+	b = 1.2
+	c = 0xdeadbeef
+	d = '\xdeadbeef'
+	print a, b, c
+	print a+b
+	print 
+
+	return
+
+
+if __name__ == "__main__":
+    test_var()
