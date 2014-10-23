b06de745e9bfff5ddba540b5d1ca0f41832c34c3
klonman <klonman@gmail.com>
2014-10-23 14:17:49 -0400

    update

--- utils/networking/py_client.py
+++ utils/networking/py_client.py
@@ -2,14 +2,14 @@ import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
 #host = socket.gethostname() # Get local machine name
-host = 'localhost'
-port = 9999                # Reserve a port for your service.
 
-print host
+host = '128.118.102.44'
+port = 9999                
+
 s.connect((host, port))
-str_1 = 16*'\x61'
+str_1 = 'hello world'
 s.send(str_1)
 print s.recv(1024)
-s.send(str_1)
 print s.recv(1024)
+
 s.close()                     # Close the socket when done
--- utils/networking/py_server.py
+++ utils/networking/py_server.py
@@ -9,29 +9,30 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
     override the handle() method to implement communication to the
     client.
     """
-    msg_pool =  [
-                    "Hacking is fun", 
-                    "We do not have final for this course, \
-                    but there will be a few challenges for you to solve",
-                    "We did a good job in the last CTF hacking competetion!", 
-                    "We will keep doing CTFs in the spring semester.",
-                    "Hacking as a career! Check out the options: \
-                    http://www.cs.gwu.edu/academics/graduate_programs/master/cybersecurity/cybersecurity-jobs"    
-                ]
 
     def handle(self):
+        self.msg_pool =  [
+                        "Hacking is fun", 
+                        "We do not have final for this course, \
+                        but there will be a few challenges for you to solve",
+                        "We did a good job in the last CTF hacking competetion!", 
+                        "We will keep doing CTFs in the spring semester.",
+                        "Hacking as a career! Check out the options: \
+                        http://www.cs.gwu.edu/academics/graduate_programs/master/cybersecurity/cybersecurity-jobs"    
+                    ]
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
         print "{} wrote:".format(self.client_address[0])
         print self.data
         # just send back the same data, but upper-cased
-        self.request.sendall("Your message in caps:\t"self.data.upper())
-        self.request.sendall("A random message from server: \n" + msg_pool[randint(0, len(msg_pool)-1)])
+        self.request.sendall("Your message in caps:\t" + self.data.upper())
+        # send another message
+        self.request.sendall("A random message from server: \n" + self.msg_pool[randint(0, len(self.msg_pool)-1)])
 
 
 if __name__ == "__main__":
     HOST, PORT = "128.118.102.44", 9999
-
+    #HOST, PORT = "localhost", 9999
     # Create the server, binding to localhost on port 9999
     server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
     print 'Server started!'
